89cb104384b930c9e9746fdc5daa278e047df142
guchengjie <25757620+guchengjie@users.noreply.github.com>
2019-12-05 22:40:01 +0800

    常用布局

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'src/textWidget.dart';
 import 'src/formWidget.dart';
 import 'src/LinearLayout.dart';
 import 'src/FlexLayout.dart';
+import 'src/flowLayout.dart';
+import 'src/StackLayout.dart';
 
 void main() => runApp(new MyApp());
 
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.blue, // 应用主题
       ),
-      initialRoute: 'flexLayout', // 设置首页的路由key为“/”
+      initialRoute: 'stackLayout', // 设置首页的路由key为“/”
       routes: { // 路由表
         // 'new_route': (context) => NewRoute(text: ModalRoute.of(context).settings.arguments),
         '/': (context) => new CalculatorPage(title: 'Flutter Demo Home Page'), // 应用首页路由
@@ -24,6 +26,8 @@ class MyApp extends StatelessWidget {
         'formWidget': (context) => new FormWiTextdget(),
         'linearLayout': (context) => new LinearLayout(),
         'flexLayout': (context) => new FlexLayout(),
+        'flowLayout': (context) => new FlowLayout(),
+        'stackLayout': (context) => new StackLayout(),
       },
       // 当调用Navigator.pushNamed(...)打开命名路由时，如果指定的路由名在路由表中已注册，则会调用路由表中的builder函数来生成路由组件；
       // 如果路由表中没有注册，才会调用onGenerateRoute来生成路由
