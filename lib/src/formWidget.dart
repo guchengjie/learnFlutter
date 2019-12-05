555e223a749d929b0ae95f9e53b89234f0a5bc28
guchengjie <25757620+guchengjie@users.noreply.github.com>
2019-12-05 19:21:42 +0800

    添加图片资源的引用

--- lib/src/formWidget.dart
+++ lib/src/formWidget.dart
@@ -53,6 +53,79 @@ class FormWiTextdget extends StatelessWidget {
                 ),
               ],
             ),
+            Row(
+              children: <Image>[
+                Image(
+                  image: AssetImage('asset/images/avatar.jpg'),
+                  width: 100.0,
+                  height: 50.0,
+                  fit: BoxFit.fill, // 会拉伸填充满显示空间，图片本身长宽比会发生变化，图片会变形。
+                  color: Colors.blue,
+                  colorBlendMode: BlendMode.difference,
+                ),
+                Image.asset(
+                  'asset/images/avatar.jpg',
+                  width: 100.0,
+                  height: 50.0,
+                  fit: BoxFit.cover, // 会按图片的长宽比放大后居中填满显示空间，图片不会变形，超出显示空间部分会被剪裁。
+                ),
+                Image.network(
+                  'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2531384262,3699915741&fm=26&gp=0.jpg',
+                  width: 100.0,
+                  height: 50.0,
+                  fit: BoxFit.contain, // 这是图片的默认适应规则，图片会在保证图片本身长宽比不变的情况下缩放以适应当前显示空间，图片不会变形。
+                ),
+                Image(
+                  image: AssetImage('asset/images/avatar.jpg'),
+                  width: 50.0,
+                  height: 50.0,
+                  fit: BoxFit.fitWidth, // 图片的宽度会缩放到显示空间的宽度，高度会按比例缩放，然后居中显示，图片不会变形，超出显示空间部分会被剪裁。
+                ),
+              ],
+            ),
+            Row(
+              children: <Image>[
+                Image(
+                  image: AssetImage('asset/images/avatar.jpg'),
+                  width: 100.0,
+                  height: 50.0,
+                  fit: BoxFit.fitHeight, // 图片的高度会缩放到显示空间的高度，宽度会按比例缩放，然后居中显示，图片不会变形，超出显示空间部分会被剪裁。
+                ),
+                Image(
+                  image: AssetImage('asset/images/avatar.jpg'),
+                  width: 100.0,
+                  height: 50.0,
+                  fit: BoxFit.scaleDown,
+                ),
+                Image(
+                  image: AssetImage('asset/images/avatar.jpg'),
+                  width: 100.0,
+                  height: 50.0,
+                  fit: BoxFit.fill, // 填充满显示空间
+                ),
+                Image(
+                  image: AssetImage('asset/images/avatar.jpg'),
+                  width: 50.0,
+                  height: 50.0,
+                  fit: BoxFit.none, // 图片没有适应策略，会在显示空间内显示图片，如果图片比显示空间大，则显示空间只会显示图片中间部分
+                ),
+              ]
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  '\uE914\uE000\uE90D',
+                  style: TextStyle(
+                    fontFamily: "MaterialIcons",
+                    fontSize: 24.0,
+                    color: Colors.green
+                  ),
+                ),
+                Icon(Icons.accessible,color: Colors.green,),
+                Icon(Icons.error,color: Colors.green,),
+                Icon(Icons.fingerprint,color: Colors.green,),
+              ],
+            )
           ],
         )
       ),
